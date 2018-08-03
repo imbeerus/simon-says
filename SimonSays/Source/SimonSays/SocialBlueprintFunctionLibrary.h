bd90cab2e98000a27aa7ed6c7a99332c2ee63d2f
Ivan <izinovyev42@gmail.com>
2018-08-03 11:35:23 +0300

    Yet another changes

--- SimonSays/Source/SimonSays/SocialBlueprintFunctionLibrary.h
+++ SimonSays/Source/SimonSays/SocialBlueprintFunctionLibrary.h
@@ -18,14 +18,8 @@ class SIMONSAYS_API USocialBlueprintFunctionLibrary : public UBlueprintFunctionL
 	static FString BuildShareGoogleUrl(FString url, FString text);
 
 	UFUNCTION(BlueprintCallable, Category = "Share")
-	static FString BuildShareFacebookUrl(FString url);
-
-	UFUNCTION(BlueprintCallable, Category = "Share")
 	static FString BuildShareRedditUrl(FString url, FString title);
 
 	UFUNCTION(BlueprintCallable, Category = "Share")
 	static FString BuildShareTwitterUrl(FString url, FString text, FString hashtags);
-
-	UFUNCTION(BlueprintCallable, Category = "Share")
-	static FString BuildShareTelegramUrl(FString url, FString text);
 };
